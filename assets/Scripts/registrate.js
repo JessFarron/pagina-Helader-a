1824cc757c0449cbed2d68da2d85c4e1fc8e3e63
almber <almaberenice123@gmail.com>
2023-05-19 14:00:21 -0600

    agregue links

---
+++ assets/Scripts/registrate.js
@@ -0,0 +1,8 @@
+const validarFormulario = () => {
+    const nombre = document.getElementById("nom");
+    const correo = document.getElementById("email");
+    const contrasena = document.getElementById("contrasena");
+    const confirContrasena = document.getElementById("confir-contrasena");
+    const telefono = document.getElementById("tel");
+    const direccionEnvio = document.getElementById("dir-envio");
+}
