2aa09dab63bd50799d8249716084e034fb04c5eb
Jessica Sanchez <jessamesan@hotmail.com>
2023-05-19 10:50:07 -0600

    Añadimos objetos de helados

---
+++ assets/Scripts/productos.js
@@ -0,0 +1,40 @@
+const productos = [
+    {
+        categoria: "helados",
+        nombre: "chocolate",
+        descripcion: "Es de chocolate",
+        precio: "35",
+        url: "assets/image/imgs-helado/helado_choco.jpg",
+    },
+    {
+        categoria: "helados",
+        nombre: "oreo",
+        descripcion: "Es de galleta oreo",
+        precio: "35",
+        url: "assets/image/imgs-helado/helado_oreo.jpg",
+    },
+    {
+        categoria: "helados",
+        nombre: "vainilla",
+        descripcion: "Es de vainilla",
+        precio: "35",
+        url: "assets/image/imgs-helado/helado_vainilla.jpg",
+    },
+
+    {
+        categoria: "helados",
+        nombre: "Baileys",
+        descripcion: "Es de Premium Baileys",
+        precio: "45",
+        url: "assets/image/imgs-helado/helado_baileys.jpg",
+    },
+    
+
+
+
+
+
+
+]
+
+
