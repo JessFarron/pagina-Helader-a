92f631dfba59c55f217dafc1589f8252a06731b7
jp.figueroamoran <jp.figueroamoran@gmail.com>
2023-05-22 10:36:49 -0600

    Feat: confirmacion de contraseña

--- assets/Scripts/registrate.js
+++ assets/Scripts/registrate.js
@@ -1,8 +1,72 @@
-const validarFormulario = () => {
-    const nombre = document.getElementById("nom");
-    const correo = document.getElementById("email");
-    const contrasena = document.getElementById("contrasena");
-    const confirContrasena = document.getElementById("confir-contrasena");
-    const telefono = document.getElementById("tel");
-    const direccionEnvio = document.getElementById("dir-envio");
-}
+function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var correo = document.getElementById('correo').value;
+    var contrasena = document.getElementById('contrasena').value;
+    var confirContrasena = document.getElementById('confirContrasena').value;
+    var telefono = document.getElementById('telefono').value;
+    var direccionEnvio = document.getElementById('direccionEnvio').value;
+    
+    // Expresión regular para validar el formato de correo electrónico
+    var formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    // Expresión regular para validar la contraseña (al menos 6 caracteres)
+    var formatoContrasena = /^.{6,}$/;
+    
+    // Validación del nombre
+    if (nombre.trim() === '') {
+      alert('Por favor, ingresa tu nombre.');
+      return false;
+    }
+    
+    // Validación del correo electrónico
+    if (!formatoCorreo.test(correo)) {
+      alert('Por favor, ingresa un correo electrónico válido.');
+      return false;
+    }
+    
+    // Validación de la contraseña
+    if (!formatoContrasena.test(contrasena)) {
+      alert('Por favor, ingresa una contraseña de al menos 6 caracteres.');
+      return false;
+    }
+    
+    // Validación de la confirmación de contraseña
+    if (contrasena !== confirContrasena) {
+      alert('Las contraseñas no coinciden. Por favor, verifica.');
+      return false;
+    }
+    
+    // Validación del teléfono
+    if (telefono.trim() === '') {
+      alert('Por favor, ingresa tu número de teléfono.');
+      return false;
+    }
+    
+    // Validación de la dirección de envío
+    if (direccionEnvio.trim() === '') {
+      alert('Por favor, ingresa tu dirección de envío.');
+      return false;
+    }
+    
+    // Envío del formulario si todas las validaciones son exitosas
+    alert('Registro exitoso!');
+    return true;
+  }
+  
+  function mostrarContrasena() {
+    var inputContrasena = document.getElementById('contrasena');
+    var inputConfirContrasena = document.getElementById('confirContrasena');
+
+    if (inputContrasena.type === 'password') {
+      inputContrasena.type = 'text';
+    } else {
+      inputContrasena.type = 'password';
+    }
+
+    if (inputConfirContrasena.type === 'password') {
+      inputConfirContrasena.type = 'text';
+    } else {
+      inputConfirContrasena.type = 'password';
+    }
+  }
+  
