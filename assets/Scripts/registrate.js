30f18103c79145eb7902cdccb142e00b9570eb53
Abraham Castillo <abrahamcastillo090@gmail.com>
2023-05-24 22:45:16 -0600

    ci:guardado en localstorage

--- assets/Scripts/registrate.js
+++ assets/Scripts/registrate.js
@@ -1,18 +1,18 @@
 
 
-function validarFormulario() {
-  var nombre = document.getElementById('nombre').value;
-  var correo = document.getElementById('correo').value;
-  var contrasena = document.getElementById('contrasena').value;
-  var confirContrasena = document.getElementById('confirContrasena').value;
-  var telefono = document.getElementById('telefono').value;
-  var direccionEnvio = document.getElementById('direccionEnvio').value;
+const validarFormulario = () => {
+  let nombre = document.getElementById('nombre').value;
+  let correo = document.getElementById('correo').value;
+  let contrasena = document.getElementById('contrasena').value;
+  let confirContrasena = document.getElementById('confirContrasena').value;
+  let telefono = document.getElementById('telefono').value;
+  let direccionEnvio = document.getElementById('direccionEnvio').value;
 
   // Expresión regular para validar el formato de correo electrónico
-  var formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   // Expresión regular para validar la contraseña (al menos 6 caracteres)
-  var formatoContrasena = /^.{6,}$/;
+  let formatoContrasena = /^.{6,}$/;
 
   // Validación del nombre
   if (nombre.trim() === '') {
@@ -55,75 +55,53 @@ function validarFormulario() {
     alert('Por favor, ingresa tu dirección de envío.');
     return false;
   }
-
-  // Envío del formulario si todas las validaciones son exitosas
-  mostrarVentanaEmergente();
   return true;
 }
 
-function guardarDatos() {
-  var nombre = document.getElementById('nombre').value;
-  var correo = document.getElementById('correo').value;
-  var telefono = document.getElementById('telefono').value;
+const datosRegistro = [];
 
-  // Crear un objeto con los datos a guardar
-  var datos = {
-    nombre: nombre,
-    correo: correo,
-    telefono: telefono
-  };
+const guardarDatos = (event) => {
+  if (validarFormulario()) {
+    alert("Registro exitoso");
+    event.preventDefault();
+    let nombre = document.getElementById('nombre').value;
+    let correo = document.getElementById('correo').value;
+    let telefono = document.getElementById('telefono').value;
 
-  // Guardar los datos en el localStorage como una cadena JSON
-  localStorage.setItem('datosRegistro', JSON.stringify(datos));
+    // Crear un objeto con los datos a guardar
+    const datos = {
+      nombre: nombre,
+      correo: correo,
+      telefono: telefono
+    };
+    datosRegistro.push(datos);
+    console.log(datosRegistro);
+    // Guardar los datos en el localStorage como una cadena JSON
+    localStorage.setItem('datosRegistro', JSON.stringify(datosRegistro));
+  }
 }
 
-function mostrarVentanaEmergente() {
-  // Crear un elemento de div para la ventana emergente
-  var ventanaEmergente = document.createElement('div');
-  ventanaEmergente.className = 'ventana-emergente';
-  ventanaEmergente.textContent = 'Registro exitoso';
+function mostrarContrasena() {
+  let inputContrasena = document.getElementById('contrasena');
+  let inputConfirContrasena = document.getElementById('confirContrasena');
 
-  // Agregar la ventana emergente al cuerpo del documento
-  document.body.appendChild(ventanaEmergente);
-
-  // Lanzar el confetti utilizando la biblioteca confetti-js
-  confetti.start();
-
-  // Después de unos segundos, eliminar la ventana emergente y detener el confetti
-  setTimeout(function () {
-    document.body.removeChild(ventanaEmergente);
-    confetti.stop();
-    }, 5000);
-    }
-    
-    function mostrarContrasena() {
-    var inputContrasena = document.getElementById('contrasena');
-    var inputConfirContrasena = document.getElementById('confirContrasena');
-    
-    if (inputContrasena.type === 'password') {
+  if (inputContrasena.type === 'password') {
     inputContrasena.type = 'text';
-    } else {
+  } else {
     inputContrasena.type = 'password';
-    }
-    
-    if (inputConfirContrasena.type === 'password') {
+  }
+
+  if (inputConfirContrasena.type === 'password') {
     inputConfirContrasena.type = 'text';
-    } else {
+  } else {
     inputConfirContrasena.type = 'password';
-    }
-    }
-    
-    // Agregar un event listener al botón de validación de formulario
-    var botonRegistro = document.getElementById('registroBtn');
-    botonRegistro.addEventListener('click', function () {
-    if (validarFormulario()) {
-    guardarDatos();
-    mostrarVentanaEmergente();
-    }
-    });
-    
-    
-    
-    
-    
-    
+  }
+}
+
+
+
+
+
+
+
+
